12217cd70bb54ef9d8dd78f03e54420d998a0495
hujiawei <jiawei.hu@chino-e.com>
2021-11-01 18:07:58 +0800

    修改了界面UI中图标和hint不对齐的问题，同时增加了密码可视性。
    Fixed the problem that the icon and hint in the UI are not aligned
    ,and achieved the visibility of the password.

--- app/src/main/java/com/bignerdranch/android/testplatflorm/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/testplatflorm/MainActivity.java
@@ -2,12 +2,16 @@ package com.bignerdranch.android.testplatflorm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.android.material.textfield.TextInputLayout;
@@ -18,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
 
     private TextInputLayout mUserInputLayout;
     private TextInputLayout mPwdInputLayout;
+    private Button mRegisterButton;
+    private Button mLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,42 +36,87 @@ public class MainActivity extends AppCompatActivity {
 
         mUserInputLayout = (TextInputLayout) findViewById(R.id.username_label);
         mPwdInputLayout = (TextInputLayout) findViewById(R.id.password_label);
+
+        EditText UsereditText = mUserInputLayout.getEditText();
+        EditText PwdeditText = mPwdInputLayout.getEditText();
+
+        UsereditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        PwdeditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = activity.getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 
     @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
         if (ev.getAction() == MotionEvent.ACTION_DOWN) {
-            View v = getCurrentFocus();
-            if (isShouldHideKeyBoard(v, ev)) {
-                hideKeyBoard(v.getWindowToken());
-            }
+            hideKeyboard(this);
         }
         return super.dispatchTouchEvent(ev);
     }
-
-    private void hideKeyBoard(IBinder windowToken) {
-        if (windowToken != null) {
-            InputMethodManager im = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-            im.hideSoftInputFromWindow(windowToken, InputMethodManager.HIDE_NOT_ALWAYS);
-        }
-    }
-
-    private boolean isShouldHideKeyBoard(View v, MotionEvent ev) {
-        if (v != null && (v instanceof EditText)) {
-            int[] l = {0, 0};
-            v.getLocationInWindow(l);
-            int left = l[0],
-                    top = l[1],
-                    bottom = top + v.getHeight(),
-                    right = left + v.getWidth();
-
-            if (ev.getX() > left && ev.getX() < right && ev.getY() > top && ev.getY() < bottom) {
-                return false;
-            } else {
-                return true;
-            }
-        }
-
-        return false;
-    }
+//
+//    private void hideKeyBoard(IBinder windowToken) {
+//        if (windowToken != null) {
+//            InputMethodManager im = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+//            im.hideSoftInputFromWindow(windowToken, InputMethodManager.HIDE_NOT_ALWAYS);
+//        }
+//    }
+//
+//    private boolean isShouldHideKeyBoard(View v, MotionEvent ev) {
+//        if (v != null && (v instanceof EditText)) {
+//            int[] l = {0, 0};
+//            v.getLocationInWindow(l);
+//            int left = l[0],
+//                    top = l[1],
+//                    bottom = top + v.getHeight(),
+//                    right = left + v.getWidth();
+//
+//            if (ev.getX() > left && ev.getX() < right && ev.getY() > top && ev.getY() < bottom) {
+//                return false;
+//            } else {
+//                return true;
+//            }
+//        }
+//
+//        return false;
+//    }
 }
